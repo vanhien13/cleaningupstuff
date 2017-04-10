e0b27c10cc4ab02bc27c37807a03ae8397fd176c
vanhien13 <vanhien13@gmail.com>
2017-04-10 12:23:25 +0700

    Create unique.R

---
+++ unique.R
@@ -0,0 +1,8 @@
+# Set the working directory
+setwd("D:/IMPUMS_LaborData")
+mydata<-read.csv("ipums.csv")
+uid_uniq <- unique(mydata$uid)
+set.seed(123)
+keep <- sample(unique(mydata$uid), 2000, replace=FALSE)
+mydata_sub <- mydata[mydata$uid %in% keep, ]
+length(unique(mydata_sub$uid)) 
