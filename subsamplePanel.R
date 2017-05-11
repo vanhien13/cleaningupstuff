e683dffd71b8be80371d00d602768fe256619f23
vanhien13 <vanhien13@gmail.com>
2017-05-11 14:18:21 +0700

    Update subsamplePanel.R

--- subsamplePanel.R
+++ subsamplePanel.R
@@ -4,5 +4,5 @@ mydata<-read.csv("ipums.csv")
 uid_uniq <- unique(mydata$uid)
 set.seed(123)
 keep <- sample(unique(mydata$uid), 2000, replace=FALSE)
-mydata_sub <- mydata[mydata$uid %in% keep, ]
+subset(mydata, uid %in% keep)
 length(unique(mydata_sub$uid)) 
